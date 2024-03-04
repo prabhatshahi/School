e19d8979a47d01247ac87a38bcdca9a3ea824c1b
Prabhat Ranjan Shahi <ranjanprabhat555@gmail.com>
2024-03-04 13:49:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./component/Navbar/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+    </>
+  );
+};
+
+export default App;
